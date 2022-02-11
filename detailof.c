6dfd20789cae949054ea0a5f95cb790a88b1c0ce
soumyadeep pani <99471698+SOUMYADEEP527@users.noreply.github.com>
2022-02-12 00:54:11 +0530

    Add files via upload

---
+++ detailof.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{ char NAME[50],HOBBY[25],BRANCH[2],SECTION[10];
+int REGDNO;
+printf ("ENTER YOUR NAME:");
+gets(NAME);
+printf("\nENTER YOUR BRANCH:");
+gets(BRANCH);
+printf("\nENTER YOUR SECTION:");
+gets(SECTION);
+printf("\nENTER YOUR HOBBY:");
+gets(HOBBY);
+printf("\nENTER YOUR REGD.NO:");
+scanf("%d",&REGDNO);
+	return 0;
+}
